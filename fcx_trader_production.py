90605f1f6d7c96d334a2812f775d25efcf9bc823
reechee <richardokonicha@gmail.com>
2020-03-28 06:09:53 +0100

    add menu and introduction to fcx production

--- fcx_trader_production.py
+++ fcx_trader_production.py
@@ -3,7 +3,8 @@ import os
 
 from flask import Flask, request
 import telebot
-# from core import fcx_markup
+from telebot import types
+from core import fcx_markup, welcome_text
 
 
 TOKEN = "746406709:AAHGsGOKxHwPOhRMdUOM5JNKsVxI2cCTbyQ"
@@ -11,25 +12,25 @@ TOKEN = "746406709:AAHGsGOKxHwPOhRMdUOM5JNKsVxI2cCTbyQ"
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-
-# keys = ReplyKeyboardMarkup(keyboard=fcx_markup, resize_keyboard=True)
-text = "this is a test suit"
+keys = types.ReplyKeyboardMarkup()
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    # bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-    chat_id = 1053579181
-    bot.send_message(chat_id, text=text)
-
-
+    chat_id = message.chat.id
+    bot.send_message(
+        chat_id,
+        text=welcome_text,
+        reply_markup=keys,
+        parse_mode="HTML"
+        )
 
 
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text, "coming soon")
 
 
 @server.route('/' + TOKEN, methods=['POST'])
