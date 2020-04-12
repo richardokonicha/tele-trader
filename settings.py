b02ca809d077399780a840b40d0d99153c40f99e
reechee <richardokonicha@gmail.com>
2020-04-12 04:25:16 +0100

    gitignore

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 
 TOKEN_TEST = "852053528:AAHL_ryUUJ1JOhenzmI0WDiayAnxxqGFmyU"
 # TOKEN = "746406709:AAHGsGOKxHwPOhRMdUOM5JNKsVxI2cCTbyQ" #fcxtrader bot
-TOKEN_PRODUCTION = "1137661512:AAEig943WBK2aCBhlrDxgpN6Tl__lpxOMUY" #FCX trading bot
+# TOKEN_PRODUCTION = "1137661512:AAEig943WBK2aCBhlrDxgpN6Tl__lpxOMUY" #FCX trading bot
 
 PUBLIC_KEY = "953b0c668c9d75c2d3da984f62a00fd269dc66c6da701250a0d7e14b52449183"
 PRIVATE_KEY = "c68f21F77B13FE4D6617EfcD0287c036da7A3aB1A5f3e870fb179E940F5839Dd"
@@ -10,6 +10,6 @@ ipn_url="https://0218d890.ngrok.io/pay"
 
 ADMIN_ID = 1053579181
 
-TEST_URL = "https://555cd460.ngrok.io/"
+TEST_URL = "https://dab34bb8.ngrok.io/"
 
 PROD_URL = 'https://fcx-bot.herokuapp.com/'
